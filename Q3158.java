cb920f47961093100d409307c058250ea6ea357a
preetidas60 <dasp69833@gmail.com>
2024-07-21 21:20:08 +0530

    added a solution of problem no. 3158

---
+++ Q3158.java
@@ -0,0 +1,18 @@
+class hey{
+    public static void main(String[] args) {
+        // int a = 10;
+        // int b = 20;
+        // {
+        //      a = 30;
+        //      int c = 25;
+        // }
+        // System.out.println(a);
+        System.out.println(fun(34, 23,78));
+    }
+    static int fun(int a, int b){
+        return a +b;
+    }
+    static int fun(int a, int b, int c){
+        return a +b+c;
+    }
+}
